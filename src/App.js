fc903e42d7e8ca757abbf5fe6239142a10c2d3fd
esapuolakka <130772020+esapuolakka@users.noreply.github.com>
2023-09-13 21:07:04 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,195 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import YouTube from 'react-youtube'
+import debounce from 'lodash.debounce'
+
+import './App.css'
+import MovieCard from './components/MovieCard'
+
+
 
 function App() {
+
+  const base_url = 'https://api.themoviedb.org/3'
+  const imageUrl = 'https://image.tmdb.org/t/p'
+  const api_key = process.env.REACT_APP_MOVIE_API_KEY
+
+  const randomStarter = Math.round(Math.random() * 20)
+
+  const [movies, setMovies] = useState([])
+  const [query, setQuery] = useState('')
+  const [selectedMovie, setSelectedMovie] = useState({})
+  const [playTrailer, setPlayTrailer] = useState(false)
+  const [isCursorActive, setIsCursorActive] = useState(false)
+  const [initialBackdropPath, setInitialBackdropPath] = useState('')
+  const [trailerAvailable, setTrailerAvailable] = useState(true)
+
+
+  // Fetching a list of movies
+const fetchMovies = async () => {
+  const type = query ? 'search' : 'discover';
+  try {
+    const { data } = await axios.get(`${base_url}/${type}/movie`, {
+      params: {
+        api_key: api_key,
+        query: query
+      }
+    });
+
+    if (data && data.results && Array.isArray(data.results) && data.results.length > 0) {
+      setMovies(data.results);
+      await setSelectedMovie(data.results[randomStarter])
+    }
+  } catch (error) {
+    console.error('Error fetching movies:', error);
+  }
+}
+
+
+  // Fetching data for a video
+  const fetchMovie = async (id) => {
+    try {
+    const { data } = await axios.get(`${base_url}/movie/${id}`, {
+      params: {
+        api_key: process.env.REACT_APP_MOVIE_API_KEY,
+        append_to_response: 'videos'
+      }
+    })
+
+    if(!data.videos || data.videos.results.length === 0) {
+      return {...data, videos: { results: [] }}
+    }
+    return data
+
+    } catch (error) {
+      console.error('Error fetching movie', error)
+      return null
+    }
+  }
+
+  const selectMovie = async (movie) => {
+    const data = await fetchMovie(movie.id)
+    if(data) {
+      setSelectedMovie(data)
+    } else {
+      console.log('Failed to fetch movie data')
+    }
+  }
+  
+  useEffect(() => {
+    fetchMovies()
+  }, [query])
+
+
+  useEffect(() => {
+    if (selectedMovie && selectedMovie.backdrop_path) {
+      setInitialBackdropPath(
+        `${imageUrl}/original/${selectedMovie.backdrop_path}`
+      );
+    }
+  }, [selectedMovie])
+
+  const renderMovies = () =>
+    movies.map(movie => (
+      <MovieCard
+        key={movie.id}
+        movie={movie}
+        imageUrl={imageUrl}
+        selectMovie={selectMovie}
+      />
+    ))
+  
+  const selectBackdrop = () => {
+    if (!selectedMovie || !selectedMovie.backdrop_path) {
+
+      return initialBackdropPath || `url(${imageUrl}/default_background_image.jpg)`
+    }
+    return `url(${imageUrl}/original/${selectedMovie.backdrop_path})`;
+  }
+
+  const renderTrailer = () => {
+    if (!selectedMovie || typeof selectedMovie.videos === 'undefined' || selectedMovie.videos.results.length === 0) {
+      return null
+    }
+    let trailer = selectedMovie.videos.results.find(video => video.name === 'Official Trailer');
+  
+    // If Official Trailer not found
+    if (!trailer) {
+      const trailerRegex = /trailer/i;
+      trailer = selectedMovie.videos.results.find(video => trailerRegex.test(video.name));
+    }
+  
+    if (trailer) {
+      const key = trailer.key;
+  
+      return (
+        <YouTube
+          className='video'
+          videoId={key}
+          opts={{
+            width: '100%',
+            height: '100%',
+            playerVars: {
+              autoplay: 1
+            }
+          }}
+        />
+      );
+    } else {
+      return <p>No trailer available</p>
+    }
+  }
+  
+
+  const handleMouseMove = () => {
+    setIsCursorActive(true)
+
+    setTimeout(() => {
+      setIsCursorActive(false)
+    }, 3000)
+  }
+
+  const debouncedFetchMovies = debounce(fetchMovies, 500)
+
+  const handleInputChange = (e) => {
+    const value = e.target.value
+    setQuery(value)
+    debouncedFetchMovies()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='header'>
+        <div className='header-content'>
+          <span className='header-title'>Movie Trailer App</span>
+        </div>
       </header>
+      <div className='hero' style={{backgroundImage: selectBackdrop()}} onMouseMove={handleMouseMove}>
+        <div className='hero-container'>
+
+          {isCursorActive && playTrailer && trailerAvailable ? (<button className='close-button' onClick={() => setPlayTrailer(false)}>Close</button>) : null}
+          {selectedMovie.videos && playTrailer ? renderTrailer() : null}
+
+          {trailerAvailable ? (<button className='button' onClick={() => setPlayTrailer(true)}>Play Trailer</button>) : null}
+          <h1 className='hero-title'>{selectedMovie.title}</h1>
+          <p className='hero-overview'>{selectedMovie.overview ? selectedMovie.overview : null}</p>
+        </div>
+      </div>
+        {movies.length === 0 ? <p className='no-movies'>No matching movies</p> :
+        <div className='container'>
+          {renderMovies()}
+        </div>
+        }
+      <div className='footer'>
+        <input
+          className='search-bar'
+          type='text'
+          placeholder='Search movies...'
+          onChange={handleInputChange}
+          value={query}
+        >
+        </input>
+      </div>
     </div>
   );
 }
---
+++ src/components/MovieCard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import noImage from './no-image2.png'
+
+export default function MovieCard({ movie, imageUrl, selectMovie }) {
+
+  return (
+    <div className='card'>
+        <div className='image-container' onClick={() => selectMovie(movie)}>
+          {movie.poster_path ?
+          <img className='image' src={`${imageUrl}/w500${movie.poster_path}`} alt={movie.title}/>
+          : 
+          <img className='image' src={noImage} alt='No image available'/>
+          }
+        </div>
+        <h5 className='movie-title'>{movie.title}</h5>
+        
+    </div>
+  )
+}
